9e2c1f40dbfa8c87818dac6e410a337c7d6f62ff
bilaltariq7479 <blackhat7479@gmail.com>
2024-05-07 04:03:28 -0700

    adding files according to new lectures

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import Logo from '../assets/logo.png'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="page_width">
+            <header>
+                <div className="logo">
+                    <img src={Logo} alt="" />
+                </div>
+                <div className="nav_links">
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about_us">About Us</Link>
+                        </li>
+                        <li>
+                            <Link to="/services">Services</Link>
+                        </li>
+                        <li className="btn_sign">
+                            <Link to="/sign_up">Sign Up</Link>
+                        </li>
+                    </ul>
+                </div>
+            </header>
+        </div>
+    )
 }
 
-export default Header;
+export default Header
